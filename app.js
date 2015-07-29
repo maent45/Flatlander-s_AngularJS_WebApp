35bb88298343d3ed18f776ff6bf2b6405b6d6bdc
Maen Terawasi <maent45@gmail.com>
2015-07-30 01:56:44 +1200

    Updated interface
    updated the interface, wrote new panel controller, and added images.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     var app = angular.module('store', [ ]);
 
     /*--- creating store controller ---*/
-    app.controller('StoreController', function(){
+    app.controller('StoreController', function () {
         /*--- set property of this controller to equals gem variable ---*/
         this.products = gems;
     });
@@ -13,17 +13,39 @@
     /*--- gem variable ---*/
     var gems = [
         {
-            name: 'Dodecahedron',
+            name: "Dodecahedron",
             price: 2.95,
-            description: 'This is a cool gem!',
-            canPurchase: false
+            description: "This is a cool gem!",
+            canPurchase: true,
+            soldOut: true,
+            image: "img/3.jpg",
+            descriptions: "This was made using diamonds.",
+            specifications: "1 Diameter in length and 3 km in width.. thats all.",
+            reviews: "..."
         },
         {
-            name: 'Pentagonal Gem',
+            name: "Pentagonal Gem",
             price: 5.95,
-            description: 'This is a cool gem!',
-            canPurchase: false
+            description: "This is a cool gem!",
+            canPurchase: true,
+            soldOut: true,
+            image: "img/2.jpg",
+            descriptions: "This was made using diamonds.",
+            specifications: "1 Diameter in length and 3 km in width.. thats all.",
+            reviews: "..."
         }
     ];
+
+    app.controller("PanelController", function () {
+        this.tab = 1;
+
+        this.selectedTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+    });
 
 })();
