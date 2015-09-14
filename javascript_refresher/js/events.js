a13cab03b9859b6e29b1313435a667054e183741
Maen Terawasi <maent45@gmail.com>
2015-09-15 03:02:38 +1200

    finished ability to edit li's
    li's can now be changed. Soon will add keypress functionality to change
    li values

--- javascript_refresher/js/events.js
+++ javascript_refresher/js/events.js
@@ -13,8 +13,17 @@ var inputs = checklist.querySelectorAll("input");
 //loop through all items to add eventListeners
 for (var i = 0; i < items.length; i++) {
     items[i].addEventListener("click", editItem);
+    inputs[i].addEventListener("blur", updateItem);
 }
 
 function editItem() {
     this.className = "edit";
+    var input = this.querySelector("input");
+    input.focus();
+    input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+    this.previousElementSibling.innerHTML = this.value;
+    this.parentNode.className = "";
 }
