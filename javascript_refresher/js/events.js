ff0bacd2f4638b2f0bbb01c03a669416ad4863ca
Maen Terawasi <maent45@gmail.com>
2015-09-15 03:10:00 +1200

    completed update item on keypress
    li values can now also be updated when pressing enter after changing the
    contained texts.

--- javascript_refresher/js/events.js
+++ javascript_refresher/js/events.js
@@ -14,6 +14,7 @@ var inputs = checklist.querySelectorAll("input");
 for (var i = 0; i < items.length; i++) {
     items[i].addEventListener("click", editItem);
     inputs[i].addEventListener("blur", updateItem);
+    inputs[i].addEventListener("keypress", itemKeypress);
 }
 
 function editItem() {
@@ -26,4 +27,11 @@ function editItem() {
 function updateItem() {
     this.previousElementSibling.innerHTML = this.value;
     this.parentNode.className = "";
+}
+
+//function to call updateItem on pressing 'Enter' key
+function itemKeypress() {
+    if (event.which === 13) {
+        updateItem.call(this);
+    }
 }
