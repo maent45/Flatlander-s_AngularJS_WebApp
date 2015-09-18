540e1e849d292bfa5bcb9d9b8045028e8d9c269a
Maen Terawasi <maent45@gmail.com>
2015-09-18 23:45:03 +1200

    removed prompt for new task
    instead of prompting a window to enter a new task name now user will
    enter new task name in an input text field

--- javascript_refresher/js/main.js
+++ javascript_refresher/js/main.js
@@ -8,13 +8,18 @@ var btnAddTask = document.getElementById("btnAddTask");
 //get toDoList reference
 var toDoList = document.getElementById("toDoList");
 
-
 //
 btnAddTask.onclick = function() {
-    //prompt user for new task name
-    var newTaskName = prompt("What's your new task?");
-    addNewTask(document.getElementById("toDoList"), newTaskName);
+    var newTaskText = document.getElementById("newTaskText");
 
+    var newTaskName = newTaskText.value;
+
+    //if task prompt is cancelled or left blank don't create new li element
+    if(!newTaskName || newTaskName == ""){
+        return false;
+    }
+
+    addNewTask(document.getElementById("toDoList"), newTaskName);
 };
 
 //function to add new task
