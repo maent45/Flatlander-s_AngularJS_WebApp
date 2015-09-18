678942a55e65ed20924b3e1d90f43703a1d3998b
Maen Terawasi <maent45@gmail.com>
2015-09-19 04:17:04 +1200

    added checkboxes to new tasks
    when new task is created a checkbox is also appended to its left side

--- javascript_refresher/js/main.js
+++ javascript_refresher/js/main.js
@@ -8,14 +8,18 @@ var btnAddTask = document.getElementById("btnAddTask");
 //get toDoList reference
 var toDoList = document.getElementById("toDoList");
 
+//reference to Task Name input box
+var newTaskText = document.getElementById("newTaskText");
+newTaskText.focus();
+
 //
 btnAddTask.onclick = function() {
-    var newTaskText = document.getElementById("newTaskText");
-
     var newTaskName = newTaskText.value;
 
     //if task prompt is cancelled or left blank don't create new li element
     if(!newTaskName || newTaskName == ""){
+        alert("You must name the task before adding it.");
+        newTaskText.focus();
         return false;
     }
 
@@ -24,8 +28,20 @@ btnAddTask.onclick = function() {
 
 //function to add new task
 function addNewTask(list, newTaskName) {
+    //create new elements
     var newTask = document.createElement("li");
-    newTask.innerText = newTaskName;
+    var checkBox = document.createElement("input");
+    var span = document.createElement("span");
 
+    checkBox.type = "checkbox";
+    //give span default text
+    span.innerText = newTaskName;
+
+    newTask.appendChild(checkBox);
+    newTask.appendChild(span);
+
+    //append the created elements
     list.appendChild(newTask);
+
+    newTaskText.select();
 }
